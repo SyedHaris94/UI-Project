136f84b1da57ca91f7ae3905bfd3ea8452292e4b
kalam-pc <ksyedharis49@gmail.com>
2017-10-29 18:55:58 +0500

    added toggle button

--- src/components/lighting.js
+++ src/components/lighting.js
@@ -5,31 +5,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Toggle from 'material-ui/Toggle';
 import SideBar from './sidebar'
 
-
-const styles = {
-    block: {
-        maxWidth: 250,
-    },
-    toggle: {
-        marginBottom: 16,
-    },
-    thumbOff: {
-        backgroundColor: '#ffcccc',
-    },
-    trackOff: {
-        backgroundColor: '#ff9d9d',
-    },
-    thumbSwitched: {
-        backgroundColor: 'red',
-    },
-    trackSwitched: {
-        backgroundColor: '#ff9d9d',
-    },
-    labelStyle: {
-        color: 'red',
-    },
-};
-
 class Lighting extends Component {
     render() {
         return (
